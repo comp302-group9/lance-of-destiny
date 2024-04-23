96d76df6d81a1532a45f73ec6bb712de9f663f5e
akaraesmen21 <akaraesmen21@ku.edu.tr>
2024-04-23 22:23:33 +0300

    Barrier movement
    barrier movement

--- domain/objects/Barrier/Barrier.java
+++ domain/objects/Barrier/Barrier.java
@@ -4,6 +4,8 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 
@@ -19,6 +21,9 @@ public abstract class Barrier {
 	protected String img;
 	protected BufferedImage image;
 	protected String message;
+	protected boolean isMoving;
+    protected int direction; // 0 for left, 1 for right (for horizontal movement)
+
 
 	// Constructor
 	public Barrier() {
@@ -34,12 +39,30 @@ public abstract class Barrier {
 		this.y=y;
 		this.width= 33 * 900 / 512;
 		this.height =  600 / 30;
+		this.isMoving = checkIfMoving();
+        if (isMoving) {
+            this.direction = new Random().nextBoolean() ? 0 : 1; // Random initial direction
+        }
 		try {
 			this.image = ImageIO.read(getClass().getResource(this.getImg()));
         } catch (IOException e) {
             e.printStackTrace();
         }
 	}
+	public boolean checkIfMoving() {
+        // Check if there is free space around the barrier in the x-axis
+        // If there is free space, return true with a probability of 0.2
+        return (new Random().nextDouble() < 0.2);
+    }
+	public boolean collidesWithBarrier(ArrayList<Barrier> barriers) {
+        Rectangle bounds = getBounds();
+        for (Barrier barrier : barriers) {
+            if (barrier != this && barrier.getBounds().intersects(bounds)) {//.equals gerekebilir
+                return true; // Collision detected
+            }
+        }
+        return false; // No collision
+    }
 	
 	public String getImg() { 
 		return img;
@@ -51,7 +74,7 @@ public abstract class Barrier {
 
 	// Method to handle the barrier being hit by the Fire Ball
 	public abstract boolean onHit();
-
+	public abstract void move();
 	// Getter methods
 	public int getX() {
 		return x;
@@ -86,6 +109,20 @@ public abstract class Barrier {
 	public Rectangle getBounds() {
         return new Rectangle(x, y, width, height); 
     }
+	 public Rectangle getFutureBounds() {
+	        // Calculate the future x-coordinate based on the current direction
+	        int futureX = x;
+	        if (isMoving) {
+	            if (direction == 0) {
+	                futureX -= L / 4; // Move left
+	            } else {
+	                futureX += L / 4; // Move right
+	            }
+	        }
+
+	        // Create and return the Rectangle object representing the future bounds
+	        return new Rectangle(futureX, y, width, height);
+	    }
 
 	public String getMessage(){
 		return message;
--- domain/objects/Barrier/SimpleBarrier.java
+++ domain/objects/Barrier/SimpleBarrier.java
@@ -10,7 +10,18 @@ public class SimpleBarrier extends Barrier {
 	public SimpleBarrier(int x, int y){
 		super(x, y);
 	}
-
+	 public void move() {
+	        if (isMoving) {
+	            // Move the barrier horizontally based on its direction
+	            if (direction == 0) {
+	                x -= 90 / 4; // Move left
+	            } else {
+	                x += 90 / 4; // Move right
+	            }
+	            // Reverse direction if about to collide with another barrier
+	            // Implement collision detection logic here
+	        }
+	    }
 	@Override
 	public boolean onHit() {
 		System.out.println("Reinforced Barrier destroyed");
