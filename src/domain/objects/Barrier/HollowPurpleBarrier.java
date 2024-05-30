a932c02fc117767a8c2b528e38ba1c10680abaa5
akaraesmen21 <akaraesmen21@ku.edu.tr>
2024-05-30 18:46:29 +0300

    UI sourcepath added

--- src/domain/objects/Barrier/HollowPurpleBarrier.java
+++ src/domain/objects/Barrier/HollowPurpleBarrier.java
@@ -4,6 +4,8 @@ public class HollowPurpleBarrier extends SimpleBarrier{
 
 	public HollowPurpleBarrier(int x, int y) {
 		super(x,y);// TODO Auto-generated constructor stub
+		this.img="/src/ui/images/purplebarrier.png";
+		
 	}
 
 }
