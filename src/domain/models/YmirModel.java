dd87f420ec646f5d401fe8fee8d13d4bb13c0eb2
Emre Kurşun <100401689+ekursun21@users.noreply.github.com>
2024-05-30 15:34:46 +0300

    Changed infinite void a bit

--- src/domain/models/YmirModel.java
+++ src/domain/models/YmirModel.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 import javax.swing.ImageIcon;
 
-import domain.objects.Spells.YmirSpell2;
+import domain.objects.Spells.InfiniteVoid;
 
 public class YmirModel {
     private ImageIcon[] coinGifs;
@@ -35,6 +35,7 @@ public class YmirModel {
     public int getFlag(){return flag;}
 
     public void castSpell() {
+        new InfiniteVoid().Activate();
         System.out.println("Bad spell");
     }
 
